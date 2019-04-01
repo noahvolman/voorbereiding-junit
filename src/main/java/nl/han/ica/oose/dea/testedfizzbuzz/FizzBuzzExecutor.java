b8153ba22b63b4bfd0b7bc3915c44aac2ff45f32
Meron Brouwer <meron.brouwer@gmail.com>
2019-04-01 10:46:18 +0200

    Added de uitwerkingen

--- src/main/java/nl/han/ica/oose/dea/testedfizzbuzz/FizzBuzzExecutor.java
+++ src/main/java/nl/han/ica/oose/dea/testedfizzbuzz/FizzBuzzExecutor.java
@@ -2,7 +2,19 @@ package nl.han.ica.oose.dea.testedfizzbuzz;
 
 public class FizzBuzzExecutor {
 
-    public String execute(int i){
-        return null;
+    public String execute(int i) {
+        var returnValue = new StringBuilder();
+
+        if (i % 3 == 0) {
+            returnValue.append("Fizz");
+        }
+        if (i % 5 == 0) {
+            returnValue.append("Buzz");
+        }
+        if (returnValue.length() == 0) {
+            returnValue.append(i);
+        }
+
+        return returnValue.toString();
     }
 }
---
+++ src/test/java/nl/han/ica/oose/dea/testedfizzbuzz/FizzBuzzExecutorTest.java
@@ -0,0 +1,59 @@
+package nl.han.ica.oose.dea.testedfizzbuzz;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class FizzBuzzExecutorTest {
+
+    private FizzBuzzExecutor sut;
+
+    @BeforeEach
+    void setup() {
+        sut = new FizzBuzzExecutor();
+    }
+
+    @Test
+    void executeWithValidIntTest() {
+        // Setup
+
+        // Test
+        var testValue = sut.execute(37);
+
+        // Verify
+        Assertions.assertEquals("37", testValue);
+    }
+
+    @Test
+    void executeWithFizzValueTest() {
+        // Setup
+
+        // Test
+        var testValue = sut.execute(3);
+
+        // Verify
+        Assertions.assertEquals("Fizz", testValue);
+    }
+
+    @Test
+    void executeWithBuzzValueTest() {
+        // Setup
+
+        // Test
+        var testValue = sut.execute(5);
+
+        // Verify
+        Assertions.assertEquals("Buzz", testValue);
+    }
+
+    @Test
+    void executeWithFizzBuzzValueTest() {
+        // Setup
+
+        // Test
+        var testValue = sut.execute(15);
+
+        // Verify
+        Assertions.assertEquals("FizzBuzz", testValue);
+    }
+}
